bedeb64afac459e4fe15ea4293d2fb5b7a359fda
ssjjbb <61027924+ssjjbb@users.noreply.github.com>
2022-07-19 16:48:04 +0300

    Update gui.py

--- RPi/gui.py
+++ RPi/gui.py
@@ -21,35 +21,35 @@ def hatch_state(n):
     hatch5 = random.randint(0, 1)
     if n == 1:
         if hatch1 == 0:
-            return "Closed"
+            return "green"
         else:
-            return "Open"
+            return "red"
     elif n == 2:
         if hatch2 == 0:
-            return "Closed"
+            return "green"
         else:
-            return "Open"
+            return "red"
     elif n == 3:
         if hatch3 == 0:
-            return "Closed"
+            return "green"
         else:
-            return "Open"
+            return "red"
     elif n == 4:
         if hatch4 == 0:
-            return "Closed"
+            return "green"
         else:
-            return "Open"
+            return "red"
     elif n == 5:
         if hatch5 == 0:
-            return "Closed"
+            return "green"
         else:
-            return "Open"
+            return "red"
     else:
         return 0
 
 
 width = 300
-height = 50
+height = 25
 
 window = tk.Tk()
 while True:
@@ -59,9 +59,9 @@ while True:
         Block for temperature values and their visuals
         ---------------------------------------------------------------------------------------------------------------
         """
-        temperature_frame1 = tk.Frame(master=window, width=width, height=height, bg="red")
+        temperature_frame1 = tk.Frame(master=window, width=width, height=height, bg="red", borderwidth=3)
         temperature_frame1.pack()
-        temperature_frame2 = tk.Frame(master=window, width=width, height=height, bg="blue")
+        temperature_frame2 = tk.Frame(master=window, width=width, height=height, bg="blue", borderwidth=3)
         temperature_frame2.pack()
         title1 = tk.Label(master=temperature_frame1, text="Temperature:", bg="red")
         title1.place(x=0, y=0)
@@ -77,40 +77,40 @@ while True:
         Block for hatch state values and their visuals 
         """
 #Frames
-        hatch1_frame = tk.Frame(master=window, width=width, height=height)
+        hatch1_frame = tk.Frame(master=window, width=width, height=height, borderwidth=3)
         hatch1_frame.pack()
-        hatch2_frame = tk.Frame(master=window, width=width, height=height)
+        hatch2_frame = tk.Frame(master=window, width=width, height=height, borderwidth=3)
         hatch2_frame.pack()
-        hatch3_frame = tk.Frame(master=window, width=width, height=height)
+        hatch3_frame = tk.Frame(master=window, width=width, height=height, borderwidth=3)
         hatch3_frame.pack()
-        hatch4_frame = tk.Frame(master=window, width=width, height=height)
+        hatch4_frame = tk.Frame(master=window, width=width, height=height, borderwidth=3)
         hatch4_frame.pack()
-        hatch5_frame = tk.Frame(master=window, width=width, height=height)
+        hatch5_frame = tk.Frame(master=window, width=width, height=height, borderwidth=3)
         hatch5_frame.pack()
 
 #Labels
-        h_title1 = tk.Label(master=hatch1_frame, text="Hatch 1 state: ")
+        h_title1 = tk.Label(master=hatch1_frame, text="Hatch 1 state: ", borderwidth=3)
         h_title1.place(x=0, y=0)
-        h_title2 = tk.Label(master=hatch2_frame, text="Hatch 2 state: ")
+        h_title2 = tk.Label(master=hatch2_frame, text="Hatch 2 state: ", borderwidth=3)
         h_title2.place(x=0, y=0)
-        h_title3 = tk.Label(master=hatch3_frame, text="Hatch 3 state: ")
+        h_title3 = tk.Label(master=hatch3_frame, text="Hatch 3 state: ", borderwidth=3)
         h_title3.place(x=0, y=0)
-        h_title4 = tk.Label(master=hatch4_frame, text="Hatch 4 state: ")
+        h_title4 = tk.Label(master=hatch4_frame, text="Hatch 4 state: ", borderwidth=3)
         h_title4.place(x=0, y=0)
-        h_title5 = tk.Label(master=hatch5_frame, text="Hatch 5 state: ")
+        h_title5 = tk.Label(master=hatch5_frame, text="Hatch 5 state: ", borderwidth=3)
         h_title5.place(x=0, y=0)
 
 #Hatches
-        h_state1 = tk.Message(master=hatch1_frame, text=hatch_state(1))
-        h_state1.place(x=120, y=0)
-        h_state2 = tk.Message(master=hatch2_frame, text=hatch_state(2))
-        h_state2.place(x=120, y=0)
-        h_state3 = tk.Message(master=hatch3_frame, text=hatch_state(3))
-        h_state3.place(x=120, y=0)
-        h_state4 = tk.Message(master=hatch4_frame, text=hatch_state(4))
-        h_state4.place(x=120, y=0)
-        h_state5 = tk.Message(master=hatch5_frame, text=hatch_state(5))
-        h_state5.place(x=120, y=0)
+        h_state1 = tk.Message(master=hatch1_frame, text="", bg=hatch_state(1))
+        h_state1.place(x=125, y=0)
+        h_state2 = tk.Message(master=hatch2_frame, text="", bg=hatch_state(2))
+        h_state2.place(x=125, y=0)
+        h_state3 = tk.Message(master=hatch3_frame, text="", bg=hatch_state(3))
+        h_state3.place(x=125, y=0)
+        h_state4 = tk.Message(master=hatch4_frame, text="", bg=hatch_state(4))
+        h_state4.place(x=125, y=0)
+        h_state5 = tk.Message(master=hatch5_frame, text="", bg=hatch_state(5))
+        h_state5.place(x=125, y=0)
 
 
         """
@@ -120,11 +120,9 @@ while True:
         window.update()
 
         """
-        Destroy stuff on screen at end of update cycle below
         ---------------------------------------------------------------------------------------------------------------
         """
-
-
+#Mass destruction below
         title1.destroy()
         title2.destroy()
 
@@ -152,7 +150,7 @@ while True:
         h_state4.destroy()
         h_state5.destroy()
 
-
         time.sleep(1)
+
     except Exception:
         window.destroy()
